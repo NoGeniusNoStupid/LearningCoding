3a7533d022ecfb9221b73cd3dc1fdc7e619e8f11
651499253@qq.com <651499253@qq.com>
2018-06-04 18:13:41 +0800

    新建项目

---
+++ LearnCode-Test/LearnCode-Test/LearnCode-Test/Example/ExtensionMethod/ExtensionMethod.cs
@@ -0,0 +1,30 @@
+﻿// ===============================================================================
+// 项目名称  :    LearnCode_Test.Example.ExtensionMethod
+// 项目描述  :    
+// ===============================================================================
+// 类名称    :    ExtensionMethod
+// 类描述    :    
+// 创建作者  :    DESKTOP-BTQS5T7/姜华健
+// 创建时间  :    2018-6-4 17:12:00
+// ===============================================================================
+// Copyright © 榕基软件 2015 . All rights reserved.
+// ===============================================================================
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LearnCode_Test.Example.ExtensionMethod
+{
+    public static class ExtensionMethod
+    {
+        public static void WriteSelf(this string strself,int year)
+        {
+            MessageBox.Show(string.Format("我是'{0}',今年'{1}'岁",strself,year));
+            
+        }
+    }
+}
---
+++ LearnCode-Test/LearnCode-Test/LearnCode-Test/FrmMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LearnCode_Test.Example.ExtensionMethod;
+namespace LearnCode_Test
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //扩展方法
+            string str = "谢佳伟";
+            str.WriteSelf(5);
+        }
+    }
+}
